51517764616298db6d092abb67b25b2240ba21e0
atlantiswarren <atlantiswarren@users.noreply.github.com>
2022-04-06 13:59:11 +0000

    deploy: af6b1c429e12a1a07db7856989233eaf6c530132

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
+var firstName = "Atlatntis";
+var lastName = "Warren";
+var age = 23;
+var favoriteFood = "Crab Legs";
 
+console.log(
+  `My name is ${firstName}. I am ${age} years old and I love to eat ${favoriteFood}.`
+);
+
+var num = 2;
+var result = num + num;
+
+console.log(result);
+
+var fullName = `${firstName} ${lastName}`;
+
+console.log(fullName);
